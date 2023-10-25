52471e3f1099b1295f27f34b24f3c9fd5d89e50e
Léo <117282572+CLipet-Leo@users.noreply.github.com>
2023-10-25 21:40:33 +0200

    All move WORKS !!!

--- 2048-C/2048-C.cpp
+++ 2048-C/2048-C.cpp
@@ -32,7 +32,7 @@ public:
 
     void draw()
     {
-        cout << "passage inside draw" << endl << endl;
+        system("cls");
         for (int i = 0; i < this->gridSize; i++)
         {
             for (int j = 0; j < this->gridSize; j++)
@@ -49,11 +49,6 @@ public:
         this->grid[1][3] = 1;
     }
 
-    void checkGrid(int yMove, int xMove)
-    {
-        
-    }
-
     void moveBlocks(int direction)
     {
         int xMove = 0;
@@ -62,28 +57,83 @@ public:
         switch (direction)
         {
         case 0:
+            // up
             yMove = -1;
-        case 1:
-            yMove = 1;
-        case 2:
             for (int ligne = 0; ligne < this->gridSize; ligne++)
             {
                 for (int colonne = 0; colonne < this->gridSize; colonne++)
                 {
                     if (this->grid[ligne][colonne] != 0)
                         continue;
+                    for (int fullCell = ligne + 1; fullCell < this->gridSize; fullCell++)
+                    {
+                        if (this->grid[fullCell][colonne] == 0)
+                            continue;
+                        this->grid[ligne][colonne] = this->grid[fullCell][colonne];
+                        this->grid[fullCell][colonne] = 0;
+                        break;
+                    }
+                }
+            }
+            break;
+        case 1:
+            // down
+            for (int ligne = this->gridSize - 1; ligne > 0; ligne--)
+            {
+                for (int colonne = 0; colonne < this->gridSize; colonne++)
+                {
+                    if (this->grid[ligne][colonne] != 0)
+                        continue;
 
+                    for (int fullCell = ligne - 1; fullCell > -1; fullCell--)
+                    {
+                        if (this->grid[fullCell][colonne] == 0)
+                            continue;
+                        this->grid[ligne][colonne] = this->grid[fullCell][colonne];
+                        this->grid[fullCell][colonne] = 0;
+                        break;
+                    }
+                }
+            }
+            break;
+        case 2:
+            // left
+            for (int ligne = 0; ligne < this->gridSize; ligne++)
+            {
+                for (int colonne = 0; colonne < this->gridSize; colonne++)
+                {
+                    if (this->grid[ligne][colonne] != 0)
+                        continue;
                     for (int fullCell = colonne + 1; fullCell < this->gridSize; fullCell++)
                     {
                         if (this->grid[ligne][fullCell] == 0)
                             continue;
                         this->grid[ligne][colonne] = this->grid[ligne][fullCell];
                         this->grid[ligne][fullCell] = 0;
+                        break;
                     }
                 }
             }
+            break;
         case 3:
-            xMove = 1;
+            // right
+            for (int ligne = 0; ligne < this->gridSize; ligne++)
+            {
+                for (int colonne = this->gridSize - 1; colonne > 0; colonne--)
+                {
+                    if (this->grid[ligne][colonne] != 0)
+                        continue;
+                    for (int fullCell = colonne - 1; fullCell > -1; fullCell--)
+                    {
+                        if (this->grid[ligne][fullCell] == 0)
+                            continue;
+                        this->grid[ligne][colonne] = this->grid[ligne][fullCell];
+                        this->grid[ligne][fullCell] = 0;
+                        break;
+                    }
+                }
+            }
+            break;
         }
 
         
@@ -93,16 +143,12 @@ public:
 class Game
 {
 public:
-    Game()
-    {
-        cout << "A toi de jouer !" << endl;
-    }
 
     int moveInput() {
+        cout << "A toi de jouer !" << endl;
         bool playing = true;
         while (playing)
         {
-
             if ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) != 0)
             {
                 cout << endl << "QUIT" << endl;
@@ -110,35 +156,36 @@ public:
             }
             if ((GetAsyncKeyState(VK_UP) & 0x8000) != 0)
             {
-                return 0;
                 while ((GetAsyncKeyState(VK_UP) & 0x8000) != 0)
                 {
                     Sleep(1);
+
                 }
+                return 0;
             }
             if ((GetAsyncKeyState(VK_DOWN) & 0x8000) != 0)
             {
-                return 1;
                 while ((GetAsyncKeyState(VK_DOWN) & 0x8000) != 0)
                 {
                     Sleep(1);
                 }
+                return 1;
             }
             if ((GetAsyncKeyState(VK_LEFT) & 0x8000) != 0)
             {
-                return 2;
                 while ((GetAsyncKeyState(VK_LEFT) & 0x8000) != 0)
                 {
                     Sleep(1);
                 }
+                return 2;
             }
             if ((GetAsyncKeyState(VK_RIGHT) & 0x8000) != 0)
             {
-                return 3;
                 while ((GetAsyncKeyState(VK_RIGHT) & 0x8000) != 0)
                 {
                     Sleep(1);
                 }
+                return 3;
             }
         }
     }

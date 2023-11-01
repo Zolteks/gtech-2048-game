dfc0cc9de358828c56ad51c21d295887bcf17f76
Léo <117282572+CLipet-Leo@users.noreply.github.com>
2023-11-01 18:02:22 +0100

    Update

--- 2048-SDL2/GameObject.cpp
+++ 2048-SDL2/GameObject.cpp
@@ -18,7 +18,7 @@ GameObject::~GameObject()
 
 void GameObject::draw(SDL_Renderer* gRenderer)
 {
-    /*SDL_Rect fillRect = { x, y, w, h };
+    SDL_Rect fillRect = { x, y, w, h };
     SDL_SetRenderDrawColor(gRenderer, 255, 192, 203, 0xFF);
-    SDL_RenderFillRect(gRenderer, fillRect);*/
+    SDL_RenderFillRect(gRenderer, &fillRect);
 }
